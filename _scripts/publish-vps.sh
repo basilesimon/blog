73c49aef7720cbba78660208960b067acfc69b8d
Basile Simon <basile@basilesimon.fr>
2021-05-21 17:49:59 +0200

    New home, new VPS

--- _scripts/publish-vps.sh
+++ _scripts/publish-vps.sh
@@ -8,7 +8,9 @@
 localjekyll="${HOME}/blog.basilesimon.fr"
 remotewebroot="/var/www/blog.basilesimon.fr/_site"
 instancehost="basilesimon.fr"
+newinstancehost="51.159.139.203"
 sshuser="basile"
+newsshuser="b"
 sshport="22"
 sshidentity="${HOME}/.ssh/id_rsa"
 
@@ -19,10 +21,16 @@ cd $localjekyll
 export PATH=/Users/silverie/.gem/ruby/2.7.0/bin:$PATH
 ${HOME}/.gem/ruby/2.7.0/bin/jekyll build
 
-echo "rsync to SSH host $instancehost ..."
+# echo "rsync to SSH host $instancehost ..."
 
+# rsync -rhp -e "ssh -p $sshport -i $sshidentity" --exclude ".*" --delete-after \
+#   --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r \
+#   $localjekyll/_site/ $sshuser@$instancehost:$remotewebroot
+
+
+echo "rsync to SSH host $newinstancehost ..."
 rsync -vrhp -e "ssh -p $sshport -i $sshidentity" --exclude ".*" --delete-after \
   --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r \
-  $localjekyll/_site/ $sshuser@$instancehost:$remotewebroot
+  $localjekyll/_site/ $newsshuser@$newinstancehost:$remotewebroot
 
 echo "SSH connection closed. Done."
